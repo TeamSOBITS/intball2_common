dbe4177217b460b4bae1d505070b00a0d75c51b3
mbzos45 <90610406+mbzos45@users.noreply.github.com>
2026-06-28 01:48:04 +0900

    [UPD] Remove fan_control entry from setup.py for moving to rsobits_intball2_gnc
    Former-commit-id: 1403a5c7c82afdf16702bb3549c18bd3f4dac53b

--- intball2_programs/setup.py
+++ intball2_programs/setup.py
@@ -36,7 +36,6 @@ setup(
             'move_relative = intball2_programs.move_relative:main',
             'cmd_vel = intball2_programs.cmd_vel:main',
             'servise_picture = intball2_programs.servise_picture:main',
-            'fan_control = intball2_programs.fan_control:main',
             'crop_pointcloud = intball2_programs.crop_pointcloud:main',
             'fix_camera_info = intball2_programs.fix_camera_info:main',
             'ply_publisher = intball2_programs.ply_publisher:main',
